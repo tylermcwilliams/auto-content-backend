878c3c1d11df4dcf0299e19754e363f886236754
tylermcwilliams <tyler.ellis.mcwilliams@gmail.com>
2023-12-07 13:18:50 +0100

    (chore): app listens on PORT 5000

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.post("/upload-video", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
